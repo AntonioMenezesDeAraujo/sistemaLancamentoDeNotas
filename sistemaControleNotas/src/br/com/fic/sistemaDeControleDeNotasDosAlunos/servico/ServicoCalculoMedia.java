def446d99bd681a55d38758c14c49fd461868cb8
antoniomenezesdearaujo <antoniomenezes20@gmail.com>
2014-09-27 05:42:31 -0300

    Servico do cálculo da média

---
+++ sistemaControleNotas/src/br/com/fic/sistemaDeControleDeNotasDosAlunos/servico/ServicoCalculoMedia.java
@@ -0,0 +1,21 @@
+package br.com.fic.sistemaDeControleDeNotasDosAlunos.servico;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServicoCalculoMedia implements Servico{
+
+	@Override
+	public void executaLogica(HttpServletRequest req, HttpServletResponse res)
+			throws Exception {
+		
+		
+	}
+
+	@Override
+	public String getNomePagina() {
+		
+		return null;
+	}
+
+}
